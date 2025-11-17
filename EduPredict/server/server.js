53dae2014d1aebda6a4181653592f816dbab9865
anshmahajan26 <anshmahajan678@gmail.com>
2025-11-17 17:26:21 +0530

    done

--- EduPredict/server/server.js
+++ EduPredict/server/server.js
@@ -16,10 +16,12 @@ app.use('/api/auth', authRoutes);
 app.use('/api/students', studentRoutes);
 app.use("/api/predict", predictRoutes);
 
+const PORT = process.env.PORT || 5000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected');
-    app.listen(5000, () => console.log('Server started on port 5000'));
+    app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
   })
   .catch((err) => console.error(err));
